0959a4b522af093eb05c02f0249b45c872a2c138
junwen061016-crypto <wwn061013@gmail.com>
2026-08-21 11:09:21 +0800

    0821

--- src/admin.js
+++ src/admin.js
@@ -1,7 +1,7 @@
 import { auth, db, rtdb } from "./firebase.js";
-import { signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-import { doc, getDoc, setDoc, updateDoc, collection, deleteDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-import { getDatabase, ref, set, get } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+import { doc, getDoc, setDoc, updateDoc, collection, deleteDoc, onSnapshot } from "firebase/firestore";
+import { signInWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
+import { ref, set, get } from "firebase/database";
 
 // 後台登入
 document.getElementById("btn-admin-login").addEventListener("click", async () => {
